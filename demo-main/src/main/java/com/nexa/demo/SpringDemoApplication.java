a8c9fac125b70a302690d81ab55dd69ba0352ef4
pan11123 <13173820978@163.com>
2025-06-29 15:42:22 +0800

    init

--- demo-main/src/main/java/com/nexa/demo/SpringDemoApplication.java
+++ demo-main/src/main/java/com/nexa/demo/SpringDemoApplication.java
@@ -1,9 +1,16 @@
 package com.nexa.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- *
  * @Author: itpan
  * @Date: 2025/6/29 14:19
  */
+@SpringBootApplication
 public class SpringDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+        System.out.println("项目启动成功");
+    }
 }
